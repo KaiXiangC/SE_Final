270a776d02dba846ed21d7e3a925353d429a5f83
KaiXiangC <james2225061@gmail.com>
2024-12-01 15:41:32 +0800

    修改app.py registration_form.py

--- app.py
+++ app.py
@@ -1,8 +1,40 @@
-from app import create_app, db
-from flask.cli import FlaskGroup
+from flask import Flask, render_template, request, redirect, url_for, flash
+from models import db, User, Issue
+from forms.registration_form import RegistrationForm, IssueForm
 
-app = create_app()
-cli = FlaskGroup(app)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SECRET_KEY'] = 'your_secret_key'
+db.init_app(app)
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(name=form.name.data, email=form.email.data, password=form.password.data)
+        form.photo.data.save(f'static/uploads/{user.name}_id.jpg')  # Save uploaded photo
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration successful!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', form=form)
+'''
+@app.route('/new_issue', methods=['GET', 'POST'])
+def new_issue():
+    form = IssueForm()
+    if form.validate_on_submit():
+        issue = Issue(name=form.name.data, description=form.description.data, category=form.category.data)
+        db.session.add(issue)
+        db.session.commit()
+        flash('Issue submitted successfully!', 'success')
+        return redirect(url_for('home'))
+    return render_template('new_issue.html', form=form)
+'''
 if __name__ == '__main__':
-    cli()
+    with app.app_context():
+        db.create_all()  # Create tables
+    app.run(debug=True)
--- app/forms/registration_form.py
+++ app/forms/registration_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, TextAreaField, FileField
+from wtforms import StringField, PasswordField, TextAreaField, SelectField, FileField
 from wtforms.validators import DataRequired, Email, Length
 
 class RegistrationForm(FlaskForm):
@@ -8,3 +8,8 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     userID = StringField('userID', validators=[DataRequired(), Length(max=50)])
     idPhoto = FileField('Profile Picture', validators=[DataRequired()])
+
+class IssueForm(FlaskForm):
+    name = StringField('Issue Name', validators=[DataRequired(), Length(max=100)])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    category = SelectField('Category', choices=[('bug', 'Bug'), ('feature', 'Feature Request')], validators=[DataRequired()])
