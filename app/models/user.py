b0c48ab62799586ff863394c3af8b144ac0ec8ae
pengleo5422 <leo542221@gmail.com>
2024-12-03 15:08:11 +0800

    修改 DB 的ID圖片跟profile 成 BLOB

--- app/models/user.py
+++ app/models/user.py
@@ -1,14 +1,15 @@
 from .. import db
 
+
 class User(db.Model):
     __tablename__ = 'user'
     userID = db.Column(db.Integer, primary_key=True)
-    idPhoto = db.Column(db.String(255), nullable=True)
+    idPhoto = db.Column(db.LargeBinary, nullable=True)
     password = db.Column(db.String(50), nullable=False)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     authenticationStatus = db.Column(db.Boolean, nullable=False, default=False)
-    profileData = db.Column(db.String(255), nullable=True)
+    profileData = db.Column(db.LargeBinary, nullable=True)
     is_admin = db.Column(db.Boolean, default=False)
 
     notifications = db.relationship('Notification', backref='user', lazy=True)
@@ -19,7 +20,7 @@ class User(db.Model):
 
     def get_id(self):
         return self.userID
-    
+
     @property
     def is_authenticated(self):
         return True
@@ -31,3 +32,15 @@ class User(db.Model):
     @property
     def is_anonymous(self):
         return False
+
+    def retrieve_user_photo(self):
+        # 從 User 表中根據 ID 查找記錄
+        user = User.query.filter_by(id=self.userID).first()
+
+        if user:
+            if user.photo:  # 確認圖片資料存在
+                return user.photo  # 返回圖片 BLOB 資料
+            else:
+                raise ValueError("No photo found for this user")
+        else:
+            raise ValueError("User not found")
