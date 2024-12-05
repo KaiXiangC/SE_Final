75514b7f9ef35092e88ce8f967bbcd41e9814e9e
KaiXiangC <james2225061@gmail.com>
2024-12-05 21:33:06 +0800

    新增main

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from config import Config
 
-# 初始化擴展
 db = SQLAlchemy()
 migrate = Migrate()
-
+login_manager = LoginManager()
 
 def create_app():
-    # 初始化 Flask 應用
-    app = Flask(__name__, template_folder='templates')
-    
-    # 設置密鑰與加載配置
+    app = Flask(__name__)
     app.config.from_object(Config)
-    
-    # 初始化資料庫和遷移
+
     db.init_app(app)
     migrate.init_app(app, db)
-
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
     # 測試組用Blueprint
     from app.routes import routes_bp
     app.register_blueprint(routes_bp, url_prefix='/test')  # 您可以根據需要更改 url_prefix
 
-    # 引入模型以確保資料庫表正確生成
-    with app.app_context():
-        from app.models import Notification, User, Vote, Favorite, Issue, Comment, Category
+    from app.routes.main import configure_routes
+    configure_routes(app)
 
     return app
